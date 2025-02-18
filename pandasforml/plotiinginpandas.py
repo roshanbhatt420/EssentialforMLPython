763821bce9b2c9799f0158d4a4c9c4ddde77999a
roshanbhatta <roshanbhatta605@gmail.com>
2025-02-18 20:32:21 +0545

    ploting is done in the pandas

---
+++ pandasforml/plotiinginpandas.py
@@ -0,0 +1,11 @@
+# pandas ploting is done using the plot() method
+# The plot() method is used to plot the data frame
+# The plot() method is a wrapper around matplotlib library
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('data.csv')
+df.plot()
+
+plt.show()
+# The plot() method is used to plot the data frame
+# The plot() method is a wrapper around matplotlib library  
