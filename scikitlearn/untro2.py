d40da9fc643c247853b0dd64db225cf2cb6a2a0e
roshanbhatta <roshanbhatta605@gmail.com>
2025-02-22 20:55:10 +0545

    dataset is ready

---
+++ scikitlearn/untro2.py
@@ -0,0 +1,13 @@
+# three thing to make good machine leaning algo in the any library
+#Representation- hypothesis spce where data preprocessing and model building is achiveied 
+#Evaluation- Essential for how you judge or prefer one ,odel vs another 
+
+#Optimization-Optimization is done after the model is made , i optimize the model accuracy, parameters and hyper parametrers are done
+# Way of evaluate how much the data is accurate
+#  valution function 1
+# supervised machine learning
+# target varible is defined
+#un supervised learning does not have any target variable
+# supervised
+# >clasiification Data 
+# >regression - if it ha
