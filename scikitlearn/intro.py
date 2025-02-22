957658fd8a3e0cb644d5d322484fed1ec8716f26
roshanbhatta <roshanbhatta605@gmail.com>
2025-02-22 20:35:44 +0545

    introduction to the scikit learn

---
+++ scikitlearn/intro.py
@@ -0,0 +1,4 @@
+# we  introduce the scikit-learn library and its basic functionalities
+#it is open  source library that is used for machine learning and data analysis
+# developed as google summer of code project by David Cournapeau in 2007
+# it is built on top of numpy, scipy and matplotlib
