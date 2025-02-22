b3be06c7d9d72e4d80bac918b295c4c7d921a3c9
roshanbhatta <roshanbhatta605@gmail.com>
2025-02-22 21:10:44 +0545

    first scikit code added

---
+++ scikitlearn/scikitlearn1.py
@@ -0,0 +1,12 @@
+# we spilt the data in he two part simply 
+#x and y
+# x is the features for the datas that cause the event
+# y contain the result 
+import sklearn
+from sklearn.datasets import load_iris
+data = load_iris()
+
+
+
+
+#in scikit learn
